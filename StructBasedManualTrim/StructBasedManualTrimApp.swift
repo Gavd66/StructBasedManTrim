69112e7cdc898e8ebe1b27a5c63f0c124f68092b
gav <gavd66@me.com>
2021-05-31 10:27:55 +1000

    Initial Commit

---
+++ StructBasedManualTrim/StructBasedManualTrimApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructBasedManualTrimApp.swift
+//  StructBasedManualTrim
+//
+//  Created by Gavin Dorward on 31/5/21.
+//
+
+import SwiftUI
+
+@main
+struct StructBasedManualTrimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
